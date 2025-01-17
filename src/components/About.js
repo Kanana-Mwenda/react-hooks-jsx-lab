7884ccead4eabf5982700dda69b047abc02969a1
Kanana-Mwenda <Lizamwenda95@gmail.com>
2025-01-17 15:02:57 +0300

    commit message

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,16 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+  <h2>About Me</h2>
+  <p>Modern web development emphasizes responsive design, 
+    ensuring websites perform seamlessly across devices, 
+    and leverages frameworks and libraries to streamline development and improve efficiency.</p>
+  <img src={image} alt="I made this"/>
+
+  </div>
+  );
 }
 
 export default About;
